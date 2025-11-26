ae1466dedd06298128eb80fbb55d4d35aee26e85
enswapwar <115549512+enswapwar@users.noreply.github.com>
2025-11-27 00:26:39 +0900

    Update server.py
    Signed-off-by: enswapwar <115549512+enswapwar@users.noreply.github.com>

--- server.py
+++ server.py
@@ -152,8 +152,8 @@ def main():
     app.router.add_static('/src/', path='./src/', show_index=False)
     app.router.add_static('/assets/', path='./assets/', show_index=False)
     app.router.add_static('/plugins/', path='./plugins/', show_index=False)
+    app.router.add_static('/songs/', path='./songs/', show_index=False)
     app.router.add_get("/disable-judge-scores.taikoweb.js", disable_judge_scores)
-    app.router.add_get('/songs/', path='./songs/', show_index=False)
 
     port = int(os.environ.get("PORT", 8080))
     web.run_app(app, host="0.0.0.0", port=port)
