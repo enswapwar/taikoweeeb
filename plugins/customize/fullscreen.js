5fbfc9d15545d7f6f30d89905b4f5ce3920e9652
enswapwar <115549512+enswapwar@users.noreply.github.com>
2026-04-07 01:46:04 +0900

    Update fullscreen.js
    Signed-off-by: enswapwar <115549512+enswapwar@users.noreply.github.com>

--- plugins/customize/fullscreen.js
+++ plugins/customize/fullscreen.js
@@ -1,1 +1,18 @@
-(function(){    function toggleFullScreen() {    if (!document.fullscreenElement) {      document.documentElement.requestFullscreen();    } else {      document.exitFullscreen();    }  }  document.addEventListener("keydown", function(e) {    if (e.key.toLowerCase() === "f" && e.ctrlKey && e.shiftKey) {      e.preventDefault();      toggleFullScreen();    }  }, false);})();
+export default class FullscreenPlugin {
+  constructor() {
+    function toggleFullScreen() {
+      if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+      } else {
+        document.exitFullscreen();
+      }
+    }
+
+    document.addEventListener("keydown", function(e) {
+      if (e.key.toLowerCase() === "f" && e.ctrlKey && e.shiftKey) {
+        e.preventDefault();
+        toggleFullScreen();
+      }
+    });
+  }
+}
